b9ffea9d068926d8c2f26d9055009ae2fd047cc4
devalmon <phlood@yandex.ru>
2017-04-01 18:29:52 +0300

    Initial Commit

---
+++ Boxes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Boxes
+//
+//  Created by Alexey Baryshnikov on 4/1/17.
+//  Copyright © 2017 Alexey Baryshnikov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
